c67bee5af08e56f0f22d7fd3f61811a05fd0788f
odilvoviccc <odiloviccc@gmail.com>
2025-01-29 11:29:07 -0800

    Init

--- dataconnect-generated/js/default-connector/esm/index.esm.js
+++
@@ -1,8 +0,0 @@
-import { , validateArgs } from 'firebase/data-connect';
-
-export const connectorConfig = {
-  connector: 'default',
-  service: 'kinopoisk-nuxt',
-  location: 'us-central1'
-};
-
--- dataconnect-generated/js/default-connector/index.cjs.js
+++
@@ -1,9 +0,0 @@
-const { , validateArgs } = require('firebase/data-connect');
-
-const connectorConfig = {
-  connector: 'default',
-  service: 'kinopoisk-nuxt',
-  location: 'us-central1'
-};
-exports.connectorConfig = connectorConfig;
-
